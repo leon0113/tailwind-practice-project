941deb50cd97a7016d9d02e5519c5f8ba8ba90e9
Leon <tahjib.leon399@gmail.com>
2023-07-11 21:03:34 +0600

    DONE

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// const btn = document.getElementById('menu-btn')
+// const nav = document.getElementById('menu')
+
+// btn.addEventListener('click', () => {
+//     btn.classList.toggle('open')
+//     nav.classList.toggle('flex')
+//     nav.classList.toggle('hidden')
+// })
+
+
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open')
+  nav.classList.toggle('flex')
+  nav.classList.toggle('hidden')
+})
